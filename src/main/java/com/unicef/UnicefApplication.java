9fd97d11c8d0dff5a19b6d07d8cf5c63474c0371
devWonil <oneill91@naver.com>
2022-11-03 11:44:20 +0900

    프로젝트 시작

---
+++ src/main/java/com/unicef/UnicefApplication.java
@@ -0,0 +1,13 @@
+package com.unicef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnicefApplication.class, args);
+	}
+
+}
