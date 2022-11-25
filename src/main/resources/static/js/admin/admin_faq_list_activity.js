4c21c3f26e2d123a0728afb092c2604b139e8054
devWonil <102580743+devWonil@users.noreply.github.com>
2022-11-25 12:58:46 +0900

    faq 클릭이벤트 완료

--- src/main/resources/static/js/admin/admin_faq_list_activity.js
+++ src/main/resources/static/js/admin/admin_faq_list_activity.js
@@ -52,4 +52,18 @@ function showFaqList(faqList){
             `;
         };
     });
+
+    $('.faq-list-header > dl').click(function(){
+        // 리스트 글자를 클릭했을 때
+        if($(this).siblings().hasClass("invisible") === false){
+            // 만약에 상세 페이지가 있다면
+            $(this).siblings().addClass('invisible');
+            // 상세 페이지를 닫고
+        }else{ 
+            $('.faq-list-header .faq-detail').addClass('invisible');
+            // 상세페이지를 닫으면
+            $(this).siblings().removeClass('invisible');
+            // 상세페이지를 보이지 않게 하라
+        }
+    });
 }
